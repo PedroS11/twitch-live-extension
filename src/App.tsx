1c9a93ab3e8e845ac3bbdff3ec779e9964906997
Pedro Silva <pedrosilva1137@gmail.com>
2021-01-06 18:08:44 +0000

    Fix dependency vulnerability (#8)
    * Upgrade versions to fix vulnerabilities
    
    * Fix linting
    
    * Update changelog

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { LiveStreamPage } from './views/LiveStreamPage/LiveStreamPage';
 import { Header } from './views/Common/Header/Header';
-import { Paper } from '@material-ui/core';
 
 function App() {
     return (
--- src/views/SettingsPage/SettingsPage.tsx
+++ src/views/SettingsPage/SettingsPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Divider, List, ListSubheader } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { SettingsFooter } from './SettingsFooter/SettingsFooter';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '../../store/reducers/twitchReducer';
