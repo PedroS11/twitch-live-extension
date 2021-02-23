cca5b40dee30169dc3d6850ad3c72593d16d4b20
Pedro Oliveira Silva <psilva@dunelm.com>
2021-02-23 21:19:19 +0000

    removed old tests

--- src/infrastructure/twitch/__mocks__/axios.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosStatic } from 'axios';
-
-const mockAxios: AxiosStatic = jest.genMockFromModule('axios');
-// this is the key to fix the axios.create() undefined error!
-mockAxios.create = jest.fn(() => mockAxios);
-
-export default mockAxios;
