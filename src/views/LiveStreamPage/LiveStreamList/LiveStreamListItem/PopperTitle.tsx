e580e4831d3e94a4dc0139d65a767f3b804011c4
Pedro Oliveira Silva <psilva@dunelm.com>
2022-05-13 22:56:23 +0100

    Remove wrong changelog file

--- src/views/LiveStreamPage/LiveStreamList/LiveStreamListItem/PopperTitle.tsx
+++ src/views/LiveStreamPage/LiveStreamList/LiveStreamListItem/PopperTitle.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { createStyles, Paper, Popper, Theme, Typography } from '@material-ui/core';
+import { createStyles, Paper, Popper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
     createStyles({
         root: {
             pointerEvents: 'none',
