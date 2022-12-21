97c2af35eeea4e35cb397747355e8bf8f3c1794b
Pedro Oliveira Silva <psilva@dunelm.com>
2022-12-21 18:51:36 +0000

    Remove unused await, add libs to tsconfig, add description to package.json

--- src/infrastructure/background/messageWrapper.ts
+++ src/infrastructure/background/messageWrapper.ts
@@ -2,23 +2,23 @@ import { BackgroundMessage } from "../../domain/background/backgroundMessage";
 import { MESSAGE_TYPES } from "../../domain/background/constants";
 
 export const sendEnableNotificationMessage = async () =>
-	await chrome.runtime.sendMessage({
+	chrome.runtime.sendMessage({
 		type: MESSAGE_TYPES.ENABLE_NOTIFICATIONS,
 	} as BackgroundMessage);
 
 export const sendDisableNotificationMessage = async () =>
-	await chrome.runtime.sendMessage({
+	chrome.runtime.sendMessage({
 		type: MESSAGE_TYPES.DISABLE_NOTIFICATIONS,
 	} as BackgroundMessage);
 
 export const sendGetTokenMessage = async (prompt = false): Promise<void> =>
-	await chrome.runtime.sendMessage({
+	chrome.runtime.sendMessage({
 		type: MESSAGE_TYPES.GET_TOKEN,
 		data: { prompt },
 	} as BackgroundMessage);
 
 export const updateBadgeIcon = async (nrStreams: number) =>
-	await chrome.runtime.sendMessage({
+	chrome.runtime.sendMessage({
 		type: MESSAGE_TYPES.UPDATE_BADGE_ICON,
 		data: { nrStreams },
 	} as BackgroundMessage);
