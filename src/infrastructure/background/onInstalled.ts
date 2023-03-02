5ca833452994cf345eca8db2728d06c5c0d7a1d7
Pedro Silva <9307951+PedroS11@users.noreply.github.com>
2023-03-02 19:57:49 +0000

    Fix bug where change logs were not openning (#56)

--- src/infrastructure/background/onInstalled.ts
+++ src/infrastructure/background/onInstalled.ts
@@ -3,27 +3,28 @@ import InstalledDetails = chrome.runtime.InstalledDetails;
 import OnInstalledReason = chrome.runtime.OnInstalledReason;
 
 // Open a tab with the changelog for the installed version
-export const processOnInstallEvents = async (details: InstalledDetails) => {
-	// Ignore chrome update events so the chrome tab doesn't open unnecessarily
-	if (details.reason === OnInstalledReason.CHROME_UPDATE) {
-		return;
-	}
+export const processOnInstallEvents = async ({ reason }: InstalledDetails) => {
+	// Guarantee that only on Twitch Live Extension installs/updates opens the change log tab
+	if (
+		reason === OnInstalledReason.UPDATE ||
+		reason === OnInstalledReason.INSTALL
+	) {
+		const { installType } = await chrome.management.getSelf();
 
-	const { installType } = await chrome.management.getSelf();
+		if (installType === "development") {
+			return;
+		}
 
-	if (installType === "development") {
-		return;
-	}
+		const { version } = chrome.runtime.getManifest();
 
-	const { version } = chrome.runtime.getManifest();
+		// @ts-ignore
+		const changelogUrl = changelogs[version];
 
-	// @ts-ignore
-	const changelogUrl = changelogs[version];
-
-	if (changelogUrl) {
-		await chrome.tabs.create({
-			url: changelogUrl,
-			active: true,
-		});
+		if (changelogUrl) {
+			await chrome.tabs.create({
+				url: changelogUrl,
+				active: true,
+			});
+		}
 	}
 };
